0653bb2e42bd24a34d6ac9cbeb7efb467d5c8a36
Alex Barth <alex@devseed>
2010-08-20 10:59:57 +0200

    Remove debug output, add comments.

--- autoreload.js
+++ autoreload.js
@@ -1,21 +1,23 @@
 
+// Start autoreloading loop.
 Drupal.behaviors.autoreload = function() {
   Drupal.autoreload_start();
 }
 
+// Fetch the autoreload file containing the time flag.
 Drupal.autoreload_start = function() {
   $.get(Drupal.settings.basePath + Drupal.settings.autoreload.file, Drupal.autoreload_callback);
 }
 
+// Callback to process the returning GET request. Compare to last time flag and
+// reload if it differs from current one.
 Drupal.autoreload_callback = function(data) {
-      console.log(data);
   if (!Drupal.autoreload_callback.last_data) {
     Drupal.autoreload_callback.last_data = data;
   }
   else {
     if (Drupal.autoreload_callback.last_data != data) {
       location.reload();
-      console.log('reload!');
     }
   }
   setTimeout(Drupal.autoreload_start, 1000);
