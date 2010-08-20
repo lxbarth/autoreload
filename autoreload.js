a48a45b2357e7095bba61ae1c6368c577fd76799
Alex Barth <alex@devseed>
2010-08-20 10:45:50 +0200

    Create loop.

---
+++ autoreload.js
@@ -0,0 +1,13 @@
+
+Drupal.autoreload_start = function() {
+  $.get(Drupal.settings.basePath + Drupal.settings.autoreload.file, Drupal.autoreload_callback);
+}
+
+Drupal.behaviors.autoreload = function() {
+  Drupal.autoreload_start();
+}
+
+Drupal.autoreload_callback = function(data) {
+  console.log(data);
+  setTimeout(Drupal.autoreload_start, 500)
+}
