36ff9e91392f3796e970623693c8ef72795c77bc
Alex Barth <alex@devseed>
2010-08-20 10:46:22 +0200

    Reorder.

--- autoreload.js
+++ autoreload.js
@@ -1,10 +1,10 @@
 
-Drupal.autoreload_start = function() {
-  $.get(Drupal.settings.basePath + Drupal.settings.autoreload.file, Drupal.autoreload_callback);
-}
-
 Drupal.behaviors.autoreload = function() {
   Drupal.autoreload_start();
+}
+
+Drupal.autoreload_start = function() {
+  $.get(Drupal.settings.basePath + Drupal.settings.autoreload.file, Drupal.autoreload_callback);
 }
 
 Drupal.autoreload_callback = function(data) {
